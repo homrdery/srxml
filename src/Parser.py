8e17b5d3d36d85c9a436fa5eb6c8392f3a5daf2c
Artem <ar.lubimov@gmail.com>
2024-06-03 18:58:05 +0300

    added tests

--- src/Parser.py
+++ src/Parser.py
@@ -1,24 +1,45 @@
 from optparse import OptionParser
 
+
 class Parser(OptionParser):
     def __int__(self):
         super().__init__(usage="usage: %prog [options] tag value filename(s)")
         # OptionParser.__init__(self,usage="usage: %prog [options] tag value filename(s)")
-    def  parse_args(self):
+
+    def parse_args(self):
 
         self.add_option("-v", action="count", dest="verbose", help="verbose", default=False)
-        self.add_option("-s", action="store_true", dest="search", help="search for tag minimum 3 args",default=True)
+        self.add_option("-s", action="store_true", dest="search", help="search for tag minimum 3 args", default=True)
         self.add_option("-r", action="store_false", dest="search", help="replace text in tag body minimum")
+        self.add_option("--user", action="store_true", dest="user_id_search", help="search by User_ID_158",
+                        default=False)
         self.options, self.args = super().parse_args()
 
+        self.params = self.prepare()
 
 
+    def prepare(self):
+        stag = "User_ID"
+        sval = self.args[0]
 
+        if self.options.search:
+            if self.options.user_id_search:
+                files = self.args[1:]
+            else:
+                stag = self.args[0]
+                sval = self.args[1]
+                files = self.args[2:]
 
-
-
-
-
-
-
-
+            return stag, sval, files
+        else:
+            if self.options.user_id_search:
+                rtag = self.args[1]
+                rval = self.args[2]
+                files = self.args[3:]
+            else:
+                stag = self.args[0]
+                sval = self.args[1]
+                rtag = self.args[2]
+                rval = self.args[3]
+                files = self.args[4:]
+            return stag, sval, rtag, rval, files
--- src/srxml.py
+++ src/srxml.py
@@ -8,6 +8,7 @@ try:
     from XML import Xml
     parser = Parser()
     parser.parse_args()
+
     logger = Logger(parser.options.verbose)
 except Exception as e:
     print(f"ERROR: {e}")
@@ -16,27 +17,25 @@ except Exception as e:
 try:
     # """Основное тело утилиты"""
     logger.msg_debug(f"Аргументы {parser.args}")
-    if len(parser.args) < 3:
-        raise Exception(f"Не хватает аргументов, передано {len(parser.args)}, а должно быть не меньше 3")
-    tag = parser.args[0]
-    value = parser.args[1]
+    # if len(parser.args) < 3:
+    #     raise Exception(f"Не хватает аргументов, передано {len(parser.args)}, а должно быть не меньше 3")
+
+
+
 
     if parser.options.search:
-        # Режим поиска
-        filenames = parser.args[2:]  # файлы в которых ищем
+
+        tag, value, filenames = parser.params
         logger.msg_info("Режим поиска")
         logger.msg_debug(f"Ищем {value} в теге {tag} в файлах {filenames}")
     else:
-        # Режим замены
-        if len(parser.args) < 5:
-            raise Exception(
-                f"Не хватает аргументов, передано {len(parser.args)}, а в режиме замены должно быть не меньше 5")
-        set_tag = parser.args[2]  # тег в котором будет произведена змена значения
-        set_value = parser.args[3]  # значение на которое мы заменим старое значение
-        filenames = parser.args[4:]  # файлы в которых меняем
+
+        tag, value, set_tag, set_value, filenames = parser.params
+
         logger.msg_info("Режим замены")
-        logger.msg_debug(
-            f"Ищем значения: {value} в теге {tag} и устанавливаем тег {set_tag} в значение {set_value} в файлах {filenames}")
+        logger.msg_debug(f"Ищем значения: {value} в теге {tag} и устанавливаем тег {set_tag} в значение {set_value} в файлах {filenames}")
+
+
     # Цикл который парсит файлы и передаёт их в опции поиска или замены
     for filename in filenames:
         logger.msg_info(filename)
